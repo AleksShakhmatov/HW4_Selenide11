ea163d22bb45005c77c296971c836c93230de368
AShakhmatov <Coluchy@yandex.ru>
2023-12-30 06:01:41 +0600

    added code of shouldFindSelenideRepositoryAtTheTop

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,32 +1,49 @@
 package github;
 
-
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.By;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideRepositorySearch {
 
-  @Test
-  void shouldFindSelenideRepositoryAtTheTop() {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.holdBrowserOpen = true;
+    }
 
-    // открыть главную страницу
-    open("https://github.com/");
-    // ввести в поле поиска selenide и нажать enter
-    $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
-    // кликнуть на первый репозиторий из списка найденых
-    $$("ul.repo-list li").first().$("a").click();
-    // проверка: заголовок selenide/selenide
-    $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
 
-
-    // ARRANGE
-    // ACT
-    // ACT
-    // (ASSERT)
-    // ACT
-    // ASSERT
-  }
+// Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+// Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+//Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+//Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
 }
